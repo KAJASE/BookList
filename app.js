6f28953c6292253eb2cc95320e4d2c09a263a455
Karen <kareninejapiassu@gmail.com>
2021-02-26 22:44:56 +0100

    Validation and Alert

--- app.js
+++ app.js
@@ -22,6 +22,20 @@ UI.prototype.addBookToList = function(book){
   `;
   list.appendChild(row);
 }
+
+//Show alerts
+UI.prototype.showAlert = function(message, className){
+  const div = document.createElement('div');
+  div.className = `alert ${className}`;
+  div.appendChild(document.createTextNode(message));
+  const container = document.querySelector('.container');
+  const form = document.querySelector('#book-form');
+  container.insertBefore(div, form);
+  setTimeout(function(){
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
+
 //Clear Fields
 UI.prototype.clearFields = function(){
   document.getElementById('title').value = '';
@@ -38,7 +52,16 @@ document.getElementById('book-form').addEventListener('submit', function(e){
   
   const book = new Book(title, author, isbn);
   const ui = new UI();
+
+  //Validate
+  if(title === '' || author === '' || isbn === ''){
+    ui.showAlert('Please fill in all fields', 'error');
+  }else{
+    ui.addBookToList(book);
+  }
+
   ui.addBookToList(book);
+  ui.showAlert('Book added!', 'success');
   //Clear fields
   ui.clearFields();
   e.preventDefault();
